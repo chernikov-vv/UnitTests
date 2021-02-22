a31bdda213d184e2797b538506e7bcf701375ae8
chernikov_vv <vell39rus@gmail.com>
2021-02-22 20:59:41 +0300

    Some fix

--- src/test/java/TestSort.java
+++ src/test/java/TestSort.java
@@ -6,48 +6,48 @@ public class TestSort {
     @Test
     public void sortPositive() {
         int[] arrayToSort = {5, 4, 3, 2, 1};
-        int[] resultArray = {1, 2, 3, 4, 5};
+        int[] expectedArray = {1, 2, 3, 4, 5};
         Sort.sort(arrayToSort);
-        Assertions.assertArrayEquals(arrayToSort, resultArray);
+        Assertions.assertArrayEquals(expectedArray, arrayToSort);
     }
 
     @Test
     public void sortNegative() {
         int[] arrayToSort = {-2, -1, -5, -4, -3};
-        int[] resultArray = {-5, -4, -3, -2, -1};
+        int[] expectedArray = {-5, -4, -3, -2, -1};
         Sort.sort(arrayToSort);
-        Assertions.assertArrayEquals(arrayToSort, resultArray);
+        Assertions.assertArrayEquals(expectedArray, arrayToSort);
     }
 
     @Test
     public void sortSortedArray() {
         int[] arrayToSort = {1, 4, 5, 7, 8, 10, 15, 19};
-        int[] resultArray = {1, 4, 5, 7, 8, 10, 15, 19};
+        int[] expectedArray = {1, 4, 5, 7, 8, 10, 15, 19};
         Sort.sort(arrayToSort);
-        Assertions.assertArrayEquals(arrayToSort, resultArray);
+        Assertions.assertArrayEquals(expectedArray, arrayToSort);
     }
 
     @Test
     public void sortPositiveAndNegative() {
         int[] arrayToSort = {-1, 4, 8, -9, 3, 11, 1, -18};
-        int[] resultArray = {-18, -9, -1, 1, 3, 4, 8, 11};
+        int[] expectedArray = {-18, -9, -1, 1, 3, 4, 8, 11};
         Sort.sort(arrayToSort);
-        Assertions.assertArrayEquals(arrayToSort, resultArray);
+        Assertions.assertArrayEquals(expectedArray, arrayToSort);
     }
 
     @Test
     public void sortBigDigit() {
         int[] arrayToSort = {-1000, 300, 980, 121, -5933, 9711, 198, -1801, 500};
-        int[] resultArray = {-5933, -1801, -1000, 121, 198, 300, 500, 980, 9711};
+        int[] expectedArray = {-5933, -1801, -1000, 121, 198, 300, 500, 980, 9711};
         Sort.sort(arrayToSort);
-        Assertions.assertArrayEquals(arrayToSort, resultArray);
+        Assertions.assertArrayEquals(expectedArray, arrayToSort);
     }
 
     @Test
     public void sortEmptyArray() {
         int[] arrayToSort = {};
-        int[] resultArray = {};
+        int[] expectedArray = {};
         Sort.sort(arrayToSort);
-        Assertions.assertArrayEquals(arrayToSort, resultArray);
+        Assertions.assertArrayEquals(expectedArray, arrayToSort);
     }
 }
